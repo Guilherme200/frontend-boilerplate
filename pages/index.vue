76dcd2f016f5d36558e70294a394f8172d6c72f3
Guilherme200 <guimatoso15@gmail.com>
2024-03-01 11:24:55 -0300

    Adjustment layout

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,47 @@
       <SearchFilter/>
       <CreateNewButton title="Nova categoria" link=""/>
     </div>
-    <DataList/>
+    <DataList :headers="headers" :data="data"/>
 </template>
 <script setup lang="ts">
 import DataList from "~/components/datalist/DataList.vue";
 import SearchFilter from "~/components/filters/SearchFilter.vue";
 import CreateNewButton from "~/components/buttons/CreateNewButton.vue";
+
+const headers = [
+  {key: 'id', label: 'ID'},
+  {key: 'name', label: 'Nome'},
+  {key: 'description', label: 'Descrição'},
+  {key: 'createdAt', label: 'Criado em'},
+  {key: 'updatedAt', label: 'Editado em'},
+]
+
+const data = [
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+  {id: '#194556', name: 'Angular', description: 'Start developing', createdAt: '12/12/2023', updatedAt: '12/12/2023'},
+]
 
 definePageMeta({
   title: 'Categorias',
