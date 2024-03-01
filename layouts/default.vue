ce2f5f664cb57d846c5d4922f211ceb6f28512fb
Guilherme200 <guimatoso15@gmail.com>
2024-03-01 08:55:19 -0300

    Adjustment layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="bg-gray-50 dark:bg-gray-800">
-    <Navbar/>
-    <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
+  <div class="bg-gray-50">
+    <Navbar class="h-[6vh]" />
+    <div class="flex overflow-hidden bg-gray-50">
       <Sidebar/>
-      <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
-        <main class="px-4 pt-6">
+      <div id="main-content" class="relative w-full overflow-y-auto bg-gray-50 h-[94vh]">
+        <main class="m-4">
           <div class="p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5">
             <div class="w-full mb-1">
               <div class="mb-4">
@@ -15,7 +15,6 @@
             </div>
           </div>
         </main>
-        <Footer/>
       </div>
     </div>
   </div>
@@ -24,7 +23,6 @@
 <script setup lang="ts">
 import Navbar from "~/layouts/template/navbar/index.vue";
 import Sidebar from "~/layouts/template/sidebar/index.vue";
-import Footer from "~/layouts/template/footer/index.vue";
 import Breadcrumb from "~/layouts/template/Breadcrumb.vue";
 
 const route = useRoute()
--- layouts/template/sidebar/index.vue
+++ layouts/template/sidebar/index.vue
@@ -1,6 +1,6 @@
 <template>
   <aside
-      class="fixed top-0 left-0 z-20 flex flex-col flex-shrink-0 hidden w-64 h-full pt-16 font-normal duration-75 lg:flex transition-width">
+      class="sticky top-0 left-0 z-20 flex flex-col flex-shrink-0 w-64 font-normal duration-75 lg:flex transition-width">
     <div
         class="relative flex flex-col flex-1 min-h-0 pt-0 bg-white border-r border-gray-200">
       <div class="flex flex-col flex-1 pt-5 pb-4 overflow-y-auto">
