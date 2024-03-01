c6f619362d28ee9931f6e0b4d848d0d93eddab90
Guilherme200 <guimatoso15@gmail.com>
2024-03-01 07:06:53 -0300

    Adjustment sidebar

--- layouts/template/sidebar/index.vue
+++ layouts/template/sidebar/index.vue
@@ -7,102 +7,55 @@
         <div class="flex-1 px-3 space-y-1 bg-white divide-y divide-gray-200">
           <ul class="pb-2 space-y-2">
             <li>
-              <NuxtLink
-                  to="/"
-                  class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group"
-              >
-                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                     fill="none" viewBox="0 0 24 24">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M4 4v15c0 .6.4 1 1 1h15M8 16l2.5-5.5 3 3L17.3 7 20 9.7"/>
-                </svg>
+              <NuxtLink to="/" class="link">
+                <ChartIcon/>
                 <span class="ml-3">Dashboard</span>
               </NuxtLink>
             </li>
           </ul>
+
           <div class="pt-2 space-y-2">
             <h3 class="flex items-center p-2 text-sm font-semibold text-gray-900">Serviços e produtos</h3>
             <ul class="pb-2 space-y-2">
               <li>
-                <NuxtLink
-                    to="#"
-                    class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group"
-                >
-                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                       xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M9.1 4H5c-.5 0-.9.4-.9.9V9c0 .5.4.9.9.9h4c.5 0 .9-.4.9-.9V5c0-.5-.4-.9-.9-.9Zm10 0H15c-.5 0-.9.4-.9.9V9c0 .5.4.9.9.9h4c.5 0 .9-.4.9-.9V5c0-.5-.4-.9-.9-.9Zm-10 10H5c-.5 0-.9.4-.9.9V19c0 .5.4.9.9.9h4c.5 0 .9-.4.9-.9v-4c0-.5-.4-.9-.9-.9Zm10 0H15c-.5 0-.9.4-.9.9V19c0 .5.4.9.9.9h4c.5 0 .9-.4.9-.9v-4c0-.5-.4-.9-.9-.9Z"/>
-                  </svg>
+                <NuxtLink to="/" class="link">
+                  <GridIcon/>
                   <span class="ml-3">Categorias</span>
                 </NuxtLink>
               </li>
               <li>
-                <NuxtLink
-                    to="#"
-                    class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group"
-                >
-                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                       xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M15 4h3c.6 0 1 .4 1 1v15c0 .6-.4 1-1 1H6a1 1 0 0 1-1-1V5c0-.6.4-1 1-1h3m0 3h6m-6 5h6m-6 4h6M10 3v4h4V3h-4Z"/>
-                  </svg>
+                <NuxtLink to="/" class="link">
+                  <NoteIcon/>
                   <span class="ml-3">Serviços</span>
                 </NuxtLink>
               </li>
               <li>
-                <NuxtLink
-                    to="#"
-                    class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group"
-                >
-                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                       xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.3L19 7h-1M8 7h-.7M13 5v4m-2-2h4"/>
-                  </svg>
+                <NuxtLink to="/" class="link">
+                  <ShoppingIcon/>
                   <span class="ml-3">Produtos</span>
                 </NuxtLink>
               </li>
             </ul>
           </div>
+
           <div class="pt-2 space-y-2">
             <h3 class="flex items-center p-2 text-sm font-semibold text-gray-900">Clientes e funcionários</h3>
             <ul class="pb-2 space-y-2">
               <li>
-                <NuxtLink
-                    to="#"
-                    class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group"
-                >
-                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                       xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
-                          d="M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3a2.5 2.5 0 1 1 2-4.5M19.5 17h.5c.6 0 1-.4 1-1a3 3 0 0 0-3-3h-1m0-3a2.5 2.5 0 1 0-2-4.5m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3c0 .6-.4 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"/>
-                  </svg>
+                <NuxtLink to="/" class="link">
+                  <UserGroupIcon/>
                   <span class="ml-3">Clientes</span>
                 </NuxtLink>
               </li>
               <li>
-                <NuxtLink
-                    to="#"
-                    class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group"
-                >
-                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                       xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
-                          d="M16 19h4c.6 0 1-.4 1-1v-1a3 3 0 0 0-3-3h-2m-2.2-4A3 3 0 0 0 19 8a3 3 0 0 0-5.2-2M3 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1c0 .6-.4 1-1 1H4a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
-                  </svg>
+                <NuxtLink to="/" class="link">
+                  <UsersIcon/>
                   <span class="ml-3">Funcionários</span>
                 </NuxtLink>
               </li>
               <li>
-                <NuxtLink
-                    to="#"
-                    class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group"
-                >
-                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                       xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14c.6 0 1-.4 1-1V7c0-.6-.4-1-1-1H5a1 1 0 0 0-1 1v12c0 .6.4 1 1 1Zm3-7h0v0h0v0Zm4 0h0v0h0v0Zm4 0h0v0h0v0Zm-8 4h0v0h0v0Zm4 0h0v0h0v0Zm4 0h0v0h0v0Z"/>
-                  </svg>
+                <NuxtLink to="/" class="link">
+                  <CalendarIcon/>
                   <span class="ml-3">Agenda</span>
                 </NuxtLink>
               </li>
@@ -115,5 +68,17 @@
 </template>
 
 <script setup lang="ts">
-
+import ChartIcon from "~/components/icons/ChartIcon.vue";
+import GridIcon from "~/components/icons/GridIcon.vue";
+import NoteIcon from "~/components/icons/NoteIcon.vue";
+import ShoppingIcon from "~/components/icons/ShoppingIcon.vue";
+import UserGroupIcon from "~/components/icons/UserGroupIcon.vue";
+import UsersIcon from "~/components/icons/UsersIcon.vue";
+import CalendarIcon from "~/components/icons/CalendarIcon.vue";
 </script>
+
+<style scoped>
+ul li .link {
+  @apply flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100
+}
+</style>
