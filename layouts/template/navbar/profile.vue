5da1724b7e61da4ac3c0f3b2531842181b16cdaa
Guilherme200 <guimatoso15@gmail.com>
2024-03-01 06:57:02 -0300

    Adjustment profile

--- layouts/template/navbar/profile.vue
+++ layouts/template/navbar/profile.vue
@@ -1,50 +1,49 @@
 <template>
   <div class="flex items-center ml-3">
     <div>
-      <button type="button"
-              class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-              id="user-menu-button-2" aria-expanded="false" data-dropdown-toggle="dropdown-2">
-        <span class="sr-only">Open user menu</span>
-        <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-             alt="user photo">
+      <button
+          @click="dropdown = !dropdown"
+          type="button"
+          class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300"
+      >
+        <img
+            class="w-8 h-8 rounded-full"
+            src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+            alt="user photo"
+        >
       </button>
     </div>
 
-    <div
-        class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
-        id="dropdown-2"
-        style="position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate3d(1419.2px, 60.8px, 0px);"
-        data-popper-placement="bottom">
-      <div class="px-4 py-3" role="none">
-        <p class="text-sm text-gray-900 dark:text-white" role="none">
-          Neil Sims
-        </p>
-        <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-          neil.sims@flowbite.com
-        </p>
+    <div v-show="dropdown" class="z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow profile">
+      <div class="px-4 py-3">
+        <p class="text-sm text-gray-900 dark:text-white">João Lucas</p>
+        <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300">joaolucas@email.com</p>
       </div>
-      <ul class="py-1" role="none">
+      <ul class="py-1">
         <li>
-          <a href="#"
-             class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-             role="menuitem">Dashboard</a>
+          <NuxtLink to="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Dashboard</NuxtLink>
         </li>
         <li>
-          <a href="#"
-             class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-             role="menuitem">Settings</a>
+          <NuxtLink to="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Configurações</NuxtLink>
         </li>
         <li>
-          <a href="#"
-             class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-             role="menuitem">Earnings</a>
-        </li>
-        <li>
-          <a href="#"
-             class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-             role="menuitem">Sign out</a>
+          <NuxtLink to="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Sair</NuxtLink>
         </li>
       </ul>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const dropdown = ref(false)
+</script>
+
+<style scoped>
+.profile {
+  position: absolute;
+  inset: 0 auto auto 0;
+  margin: 0;
+  transform: translate3d(1489.2px, 60.8px, 0px);
+}
+</style>
+
