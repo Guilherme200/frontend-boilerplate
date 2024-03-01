f3dd21e0c8448b04492e146f3cd303343635304c
Guilherme200 <guimatoso15@gmail.com>
2024-03-01 02:42:13 -0300

    Add theme

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-gray-50 dark:bg-gray-800">
+    <Navbar />
+    <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
+      <Sidebar />
+      <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
+        <main>
+          <slot />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from "~/layouts/navbar/index.vue";
+import Sidebar from "~/layouts/sidebar/index.vue";
+import Footer from "~/layouts/footer/index.vue";
+import { initFlowbite } from 'flowbite'
+
+onMounted(() => initFlowbite())
+</script>
